415183301b7338bdda4c60dd94fd8c309d511ea7
Rafael da Cunha Carboni <rafaelcarboni@MacBook-Pro-de-Rafael.local>
2025-07-03 19:22:00 -0300

    feat: adiciona seção de protocolos de segurança na Home

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,6 +9,11 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -23,16 +28,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/admin/boletins.js"
-    ],
-    "/admin/boletins/[id]/edit": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/admin/boletins/[id]/edit.js"
-    ],
-    "/admin/usuarios": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/admin/usuarios.js"
     ]
   },
   "ampFirstPages": []
